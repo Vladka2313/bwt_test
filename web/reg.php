dc6b4cda010266e263443511b9ec1f5966793677
Vladka <kot2305@ukr.net>
2016-04-11 12:23:16 +0300

    Успешный вход существующего пользователя проверка на существование пользователя в бд

--- web/reg.php
+++ web/reg.php
@@ -1,38 +1,33 @@
 <html>
-
+<meta charset="utf-8">
     <head>
-    <meta charset="utf-8">
+
     <title>Регистрация</title>
     </head>
     <body>
     <h2>Регистрация</h2>
     <form action="save_user.php" method="post">
-    <p>
-    <label>Ваше Имя:<br></label>
-    <input name="name" type="text" size="15" maxlength="15">
-    </p>
 <p>
-    <label>Ваша Фамилия:<br></label>
-    <input name="last_name" type="text" size="15" maxlength="25">
+        <label>Имя пользователя:<br></label>
+        <input name="name" type="text" size="15" maxlength="15">
     </p>
-<p>
-        <p> Выберите ваш пол </p>
-        <form>
-            <select name="sex" >
-                <option value="m"> male </option>
-                <option value="f"> famale </option>
-            </select>
-        </form>
-    </p>
-<p>
-    <label>Дата рождения:<br></label>
-    <input name="data" type="date" size="15" maxlength="15">
-    </p>
-<p>
-    <label>Email:<br></label>
-    <input name="email" type="email" size="15" maxlength="25">
-    </p>
-<p>
+        <p>
+            <label>Фамилия:<br></label>
+            <input name="last_name" type="text" size="15" maxlength="25">
+        </p>
+        <p>
+            <label>Дата рождения:<br></label>
+            <input name="date" type="text" size="15" maxlength="15">
+        </p>
+        <p>
+            <label>Ваш пол:<br></label>
+            <input name="sex" type="text" size="15" maxlength="15">
+        </p>
+        <p>
+            <label>Email:<br></label>
+            <input name="email" type="email" size="15" maxlength="25">
+        </p>
+     <p>
     <label>Login:<br></label>
     <input name="login" type="text" size="15" maxlength="25">
     </p>
@@ -40,8 +35,9 @@
     <label>Пароль:<br></label>
     <input name="password" type="password" size="15" maxlength="20">
     </p>
-
+<p>
     <input type="submit" name="submit" value="Зарегистрироваться">
-</p></form>
+    </p>
+</form>
     </body>
     </html>
--- web/save_user.php
+++ web/save_user.php
@@ -1,13 +1,16 @@
+<html>
+<meta charset="utf-8">
+</html>
 <?php
 if (isset($_POST['name'])) { $name = $_POST['name']; if ($name == '') { unset($name);} }
 if (isset($_POST['last_name'])) { $last_name = $_POST['last_name']; if ($last_name == '') { unset($last_name);} }
-if (isset($_POST['email'])) { $email = $_POST['email']; if ($email == '') { unset($email);} }
-if (isset($_POST['sex'])) { $sex = $_POST['sex']; if ($sex  == '') { unset($sex);} }
 if (isset($_POST['date'])) { $date = $_POST['date']; if ($date == '') { unset($date);} }
+if (isset($_POST['sex'])) { $sex = $_POST['sex']; if ($sex  == '') { unset($sex);} }
+if (isset($_POST['email'])) { $email = $_POST['email']; if ($email == '') { unset($email);} }
 if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);} }
 if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
 
-if (empty($login) or empty($password) or empty($name) or empty($last_name)or empty($email) or empty($date) or empty($sex))
+if (empty($name) or empty($last_name) or empty($date) or empty($sex) or empty($email) or empty($login) or empty($password)   )
 {
     exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
 }
@@ -16,6 +19,10 @@ $name = stripslashes($name);
 $name = htmlspecialchars($name);
 $last_name = stripslashes($last_name);
 $last_name = htmlspecialchars($last_name);
+$date = stripslashes($date);
+$date = htmlspecialchars($date);
+$sex = stripslashes($sex);
+$sex = htmlspecialchars($sex);
 $email = stripslashes($email);
 $email = htmlspecialchars($email);
 $login = stripslashes($login);
@@ -25,6 +32,8 @@ $password = htmlspecialchars($password);
 
 $name = trim($name);
 $last_name = trim($last_name);
+$date = trim($date);
+$sex = trim($sex);
 $email = trim($email);
 $login = trim($login);
 $password = trim($password);
@@ -32,14 +41,28 @@ $password = trim($password);
 include ("bd.php");
 //$result = $conn->query("SELECT id FROM reg WHERE login='$login'");
 $result = $conn->query('SELECT * FROM reg WHERE login= '.$conn->quote($login));
-
-$myrow = $conn->fetch_array($result,MYSQLI_NUM);
+$result->execute();
+$myrow = $result->fetchall();
+/*while( $myrow=$result->fetchAll() )
+{
+    echo $myrow['id']."-".$myrow['login']."<br>";
+}*/
+//$myrow = $conn->fetch_array($result);
+/*if ( count($result) ) {
+    foreach($result as $myrow) {
+      //  print_r($myrow);
+    }
+} else {
+    echo "Yes";
+}
+*/
+//$myrow = $conn->fetchall(PDO::FETCH_BOTH);
 
 if (!empty($myrow['id'])) {
     exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
 }
 
-$result2 = $conn->query ("INSERT INTO reg (name,last_name,email,sex,date,login,password) VALUES('$name','$last_name','$email','$sex','$date''$login','$password')");
+$result2 = $conn->query ("INSERT INTO reg (name,last_name,date,sex,email,login,password) VALUES('$name','$last_name','$date','$sex','$email','$login','$password')");
 
 if ($result2=='TRUE')
 {

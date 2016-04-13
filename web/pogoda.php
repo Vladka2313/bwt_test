00168a34933dd44cda5306201a1cb53329b8d556
Vladka <kot2305@ukr.net>
2016-04-13 20:01:23 +0300

    мелкие правки с погодой

--- web/pogoda.php
+++ web/pogoda.php
@@ -15,8 +15,16 @@ var_dump($pogoda[0]);*/
 $gismeteo = 'http://www.gismeteo.ua/city/daily/5093/';
 $content = file_get_contents ($gismeteo);
 preg_match_all ('#<div class="wsection wdata">(.*?)<div class="section bottom">#is', $content,$pogoda);
-var_dump($pogoda[0]);
+//var_dump($pogoda[0]);
 //print_r ($pogoda[0]);
 //echo $pogoda[0];
 //return $pogoda;
+echo $pogoda[0][0];
+//$a = ['test' => [['lol', 'lol2', 'lol3' => 'lol4'], 'lol5']];
+/*$a2 = $a['test'];
+$a3 = $a2[0];
+echo $a3['lol3'];
+echo  $a2[1];*/
+//echo $a ['test'][0]['lol3'] ;
+//echo $a ['test'][1];
 ?>
